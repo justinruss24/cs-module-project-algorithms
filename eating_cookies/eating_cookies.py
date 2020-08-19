4c690421e8ab26e655159d50ed07fd2a4a756f0f
Justin Russell <Justin@justins-macbook.lan>
2020-08-19 12:07:02 -0700

    done eating cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,10 +2,20 @@
 Input: an integer
 Returns: an integer
 '''
-def eating_cookies(n):
-    # Your code here
+def eating_cookies(n, arr=None):
+    # Your code 
+    # iterative, O(n) runtime, O(n) storage
+    if n <= 1:
+        return 1
+    if n == 2:
+        return 2
+    if arr == None:
+        arr = [0 for i in range(n+1)]
+    arr[0], arr[1], arr[2] = 1,1,2
+    for i in range(3,n+1):
+        arr[i] = arr[i-1] + arr[i-2] + arr[i-3]
+    return arr[n]
 
-    pass
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
